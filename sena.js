a181abc445e6cd1bc53c69ddc575eed23a446967
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-04-04 09:29:51 +0530

    loaded the images

---
+++ sena.js
@@ -0,0 +1,17 @@
+var canv = document.getElementById("canvas");
+var cont = canv.getContext("2d");
+
+/**
+ * this part for the images loading
+ */
+
+var catepilar = new Image();
+var sky       = new Image();
+var paddy     = new Image();
+var bambo     = new Image();
+
+catepilar.src = "images/catepiller.png";
+sky.src       = "images/background.png";
+paddy.src     = "images/fg.png";
+bambo.src     = "images/up.png";
+
