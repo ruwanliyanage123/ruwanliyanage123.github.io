652ee20ff456e2bb075dfc48329aa0fc4fc50229
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-04-04 09:51:06 +0530

    add draw funciton

--- sena.js
+++ sena.js
@@ -34,3 +34,50 @@ bambo.src     = "images/up.png";
      bY -=25;
      fly.play();
  }
+
+ /**
+  * this part for the cordinators
+  */
+
+  var coord = [];
+
+  coord[0] = {
+      x: canv.clientWidth,
+      y: 0
+  };
+
+
+  /**
+   * this part for the draw images
+   */
+
+   function draw(){
+      cont.drawImage(sky,0,0);
+
+      constant = paddy.height+gap;
+      cont.drawImage(paddy,coord[i].x,coord[i].y);
+      //cont.drawImage(pipeSouth,pipe[i].x,pipe[i].y+constant);
+
+      coord[i].x--;
+
+      if( coord[i].x == 125 ){
+        coord.push({
+            x : canv.width,
+            y : Math.floor(Math.random()*bambo.height)-bambo.height
+        }); 
+      }
+
+      /**
+       * this for the collision detection
+       */
+
+      if(bX + catepilar.width >= coord[i].x && bX <= coord[i].x + bambo.width && (bY <= coord[i].y + bambo.height || bY+catepilar.height >= coord[i].y+constant) || bY + catepilar.height >=  canv.height - paddy.height){
+        location.reload(); // reload the page
+      }
+
+      if(coord[i].x == 5){
+        score++;
+        scor.play();
+     }
+
+   }
