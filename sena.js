1966b57cf587aada198d77432531e4b7b5be9cf5
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-04-04 10:43:42 +0530

    add draw function

--- sena.js
+++ sena.js
@@ -1,83 +1,125 @@
+/**
+ * this part is for the define the canvas
+ */
 var canv = document.getElementById("canvas");
 var cont = canv.getContext("2d");
 
 /**
- * this part for the images loading
+ * this part is for the loading images
  */
+var catepillar  = new Image();
+var sky         = new Image();
+var paddy       = new Image();
+var upper_bambo = new Image();
+var lower_bambo = new Image();
 
-var catepilar = new Image();
-var sky       = new Image();
-var paddy     = new Image();
-var bambo     = new Image();
-
-catepilar.src = "images/catepiller.png";
-sky.src       = "images/background.png";
-paddy.src     = "images/fg.png";
-bambo.src     = "images/up.png";
+catepillar.src  = "images/catepiller.png";
+sky.src         = "images/background.png";
+paddy.src       = "images/paddy.png";
+upper_bambo.src = "images/up.png";
+//lower_bambo.src = "images/lowerb.png";
 
 /**
- * this part for the patrameter estimation for the game
+ * this part for the define the variables
  */
- var gap       = 300;
- var constant;
- var bX        = 10;
- var bY        = 150;
- var gravity   = 1.5;
- var score     = 0;
+var gap          = 585;
+var constant;
+var catepillar_X = 10;
+var catepillar_Y = 150;
+var gravity      =1.5;
+var score        = 0;
 
- /**
-  * this function for get the keyboard commands 
-  */
- document.addEventListener("keydown", moveUp);
+/**
+ * this part for get thr keyboard respponse
+ */
 
- function moveUp(){
-     bY -=25;
-     fly.play();
- }
+document.addEventListener("keydown",moveUp);
 
- /**
-  * this part for the cordinators
-  */
+function moveUp(){
+    catepillar_Y -= 25;
+}
 
-  var coord = [];
+/**
+ * this part for the define abot coordinates
+ */
+var coord = [];
 
-  coord[0] = {
-      x: canv.clientWidth,
-      y: 0
-  };
+coord[0] = {
+    x : canv.width,
+    y : 0
+};
+
+/**
+ * this part is the heart of the game
+ */
+
+function draw(){
+    
+    cont.drawImage(sky,0,0);
+    
+    
+    for(var i = 0; i < coord.length; i++){
+        
+        constant = upper_bambo.height+gap;
+        cont.drawImage(upper_bambo,coord[i].x,coord[i].y);
+        cont.drawImage(lower_bambo,coord[i].x,coord[i].y+constant);
+             
+        coord[i].x--;
+        
+        if( coord[i].x == 125 ){
+            coord.push({
+                x : canv.width,
+                y : Math.floor(Math.random()*upper_bambo.height)-upper_bambo.height
+            }); 
+        }
+
+        /**
+         * for avoid the collisions
+         */
+        
+        if( catepillar_X + catepillar.width >= coord[i].x && catepillar_X <= coord[i].x + upper_bambo.width && (catepillar_Y <= coord[i].y + upper_bambo.height || catepillar_Y+catepillar.height >= coord[i].y+constant) || catepillar_Y + catepillar.height >=  canv.height - paddy.height){
+            location.reload(); // reload the page
+        }
+        
+        if(coord[i].x == 5){
+            score++;
+        }
+        
+        
+    }
+
+    cont.drawImage(paddy,0,canv.height - paddy.height);
+    cont.drawImage(catepillar,catepillar_X,catepillar_Y);
+    catepillar_Y += gravity;
+    cont.fillStyle = "#000";
+    cont.font = "20px Verdana";
+    cont.fillText("Score : "+score,10,canv.height-20);
+    requestAnimationFrame(draw);
+    
+}
+
+draw();
 
 
-  /**
-   * this part for the draw images
-   */
 
-   function draw(){
-      cont.drawImage(sky,0,0);
 
-      constant = paddy.height+gap;
-      cont.drawImage(paddy,coord[i].x,coord[i].y);
-      //cont.drawImage(pipeSouth,pipe[i].x,pipe[i].y+constant);
 
-      coord[i].x--;
 
-      if( coord[i].x == 125 ){
-        coord.push({
-            x : canv.width,
-            y : Math.floor(Math.random()*bambo.height)-bambo.height
-        }); 
-      }
 
-      /**
-       * this for the collision detection
-       */
 
-      if(bX + catepilar.width >= coord[i].x && bX <= coord[i].x + bambo.width && (bY <= coord[i].y + bambo.height || bY+catepilar.height >= coord[i].y+constant) || bY + catepilar.height >=  canv.height - paddy.height){
-        location.reload(); // reload the page
-      }
 
-      if(coord[i].x == 5){
-        score++;
-        scor.play();
-     }
 
-   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
